2a770748d47e2af9d39c6350be1524c58eab49ed
Dianaguedes19 <1241903@isep.ipp.pt>
2025-03-23 20:08:31 +0000

    Added new folders to identify springboot project, also added new domain do domain folder and created database with postgres

---
+++ src/main/java/com/diana/taskmanagerForTeams/Domain/User.java
@@ -0,0 +1,21 @@
+package com.diana.taskmanagerForTeams.Domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+
+
+}
