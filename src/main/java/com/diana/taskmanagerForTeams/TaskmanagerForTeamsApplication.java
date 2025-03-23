81d5c102f8c51d99df07633c8d25f517eb6cd8c5
Dianaguedes19 <1241903@isep.ipp.pt>
2025-03-23 19:29:44 +0000

    first commit

---
+++ src/main/java/com/diana/taskmanagerForTeams/TaskmanagerForTeamsApplication.java
@@ -0,0 +1,13 @@
+package com.diana.taskmanagerForTeams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerForTeamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerForTeamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diana/taskmanagerForTeams/TaskmanagerForTeamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diana.taskmanagerForTeams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerForTeamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
