220d79032e3207e3db9405629e80b6fd16148ad0
Dianaguedes19 <1241903@isep.ipp.pt>
2025-03-24 11:16:44 +0000

    Added constraints to Task

--- src/main/java/com/diana/taskmanagerForTeams/Domain/Task.java
+++ src/main/java/com/diana/taskmanagerForTeams/Domain/Task.java
@@ -2,6 +2,10 @@ package com.diana.taskmanagerForTeams.Domain;
 
 import com.diana.taskmanagerForTeams.Domain.Enum.Status;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -11,18 +15,44 @@ public class Task {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    @Size(min = 4, max = 20)
     private String title;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    @Size(min = 15 , max = 150)
     private String description;
+
+    @NotEmpty
+    @NotNull
+    @NotBlank
     private LocalDate deadline;
 
+
+    @NotEmpty
+    @NotNull
+    @NotBlank
     @Enumerated(EnumType.STRING) // Save the value as text and not as number
     @Column(nullable = false) // Make it "obligate" to use
     private Status status;
 
+
+    @NotEmpty
+    @NotNull
+    @NotBlank
     @ManyToOne
     @JoinColumn(name = "assigned_user_id")
     private User userAssign;
 
+
+    @NotEmpty
+    @NotNull
+    @NotBlank
     @ManyToOne
     @JoinColumn(name = "project_id")
     private Project project;
