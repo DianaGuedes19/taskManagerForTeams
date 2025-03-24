654ce83cb5b5a58e7bea2c799202ef60b4962b21
Dianaguedes19 <1241903@isep.ipp.pt>
2025-03-24 11:17:58 +0000

    Added constraints to Team class

--- src/main/java/com/diana/taskmanagerForTeams/Domain/Team.java
+++ src/main/java/com/diana/taskmanagerForTeams/Domain/Team.java
@@ -1,6 +1,10 @@
 package com.diana.taskmanagerForTeams.Domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,14 +23,25 @@ public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @NotNull
+    @NotEmpty
+    @Size(min = 5)
     private String name;
 
+    @NotBlank
+    @NotNull
+    @NotEmpty
     @ManyToMany
     @JoinTable(name = "team_user",
             joinColumns = @JoinColumn(name = "team_id"),
             inverseJoinColumns = @JoinColumn(name = "user_id"))
     private List<User> users;
 
+    @NotBlank
+    @NotNull
+    @NotEmpty
     @OneToMany(mappedBy = "team")
     private List<Project> projects;
 
