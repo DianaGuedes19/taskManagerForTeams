8a2be71e30be83947af01e6bac1b5e7919f9c2d6
Dianaguedes19 <1241903@isep.ipp.pt>
2025-03-24 10:54:43 +0000

    Added constraints to project attributes

--- src/main/java/com/diana/taskmanagerForTeams/Domain/Project.java
+++ src/main/java/com/diana/taskmanagerForTeams/Domain/Project.java
@@ -1,6 +1,10 @@
 package com.diana.taskmanagerForTeams.Domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,13 +21,29 @@ public class Project {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @NotEmpty
+    @NotBlank
+    @Size(min = 3)
     private String name;
+
+    @NotBlank
+    @NotNull
+    @NotEmpty
+    @Size(min = 20 , max = 150)
     private String description;
 
+    @NotBlank
+    @NotNull
+    @NotEmpty
     @ManyToOne
     @JoinColumn(name = "team_id")
     private Team team;
 
+    @NotBlank
+    @NotNull
+    @NotEmpty
     @OneToMany(mappedBy = "project") // The mapped by is informing that the other class is responsible for the FK
     private List<Task> tasks;
 
