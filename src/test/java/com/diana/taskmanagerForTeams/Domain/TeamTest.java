fe36bbdccd485e9f0ca2946c0bffe6c3886fd796
Dianaguedes19 <1241903@isep.ipp.pt>
2025-03-28 12:41:47 +0000

    Added all the tests for Team domain

--- src/test/java/com/diana/taskmanagerForTeams/Domain/TeamTest.java
+++ src/test/java/com/diana/taskmanagerForTeams/Domain/TeamTest.java
@@ -23,5 +23,62 @@ class TeamTest {
         // Assert
         assertNotNull(team);
     }
+    @Test
+    void shouldSetNameProperly() {
+        // arrange
+        List<User> users = spy(new ArrayList<>());
+        List<Project> projects = spy(new ArrayList<>());
+
+        // act
+        Team team = new Team(3L, users, "Operations", projects);
+
+        // assert
+        assertEquals("Operations", team.getName());
+    }
+
+    @Test
+    void shouldFailNameWithNumbers() {
+        // arrange
+        List<User> users = spy(new ArrayList<>());
+        List<Project> projects = spy(new ArrayList<>());
+
+        // act
+        Team team = new Team(4L, users, "Team123", projects);
+
+        // assert
+        assertFalse(team.getName().matches("^[A-Za-z]+$")); }
+
+    @Test
+    void shouldFailNameWithLessThanFiveCharacters() {
+        // arrange
+        List<User> users = spy(new ArrayList<>());
+        List<Project> projects = spy(new ArrayList<>());
+
+        // act
+        Team team = new Team(5L, users, "ABc", projects);
+
+        // assert
+        assertTrue(team.getName().length() < 5);
+    }
+
+    @Test
+    void shouldSetAndGetAllFields() {
+        // arrange
+        Team team = new Team();
+        List<User> users = new ArrayList<>();
+        List<Project> projects = new ArrayList<>();
+
+        // act
+        team.setId(3L);
+        team.setName("Support");
+        team.setUsers(users);
+        team.setProjects(projects);
+
+        // assert
+        assertEquals(3L, team.getId());
+        assertEquals("Support", team.getName());
+        assertEquals(users, team.getUsers());
+        assertEquals(projects, team.getProjects());
+    }
 
 }
