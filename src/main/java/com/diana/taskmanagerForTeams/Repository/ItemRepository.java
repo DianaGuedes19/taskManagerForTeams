a1ef7551f19e0cdee62821c03aef5d5a1e6af589
Dianaguedes19 <1241903@isep.ipp.pt>
2025-03-28 15:33:25 +0000

    Ref #2: Added methods to repository that are not CRUD methods because the service class does not know how to acess databse , he asks the repository

--- src/main/java/com/diana/taskmanagerForTeams/Repository/ItemRepository.java
+++ src/main/java/com/diana/taskmanagerForTeams/Repository/ItemRepository.java
@@ -1,7 +1,12 @@
 package com.diana.taskmanagerForTeams.Repository;
 
 import com.diana.taskmanagerForTeams.Domain.Item;
+import com.diana.taskmanagerForTeams.Domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item,Long> {
+    List<Item> findByUser (User user);
+    long countItemsBoughtByUser (User user);
 }
